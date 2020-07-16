19d57d0bdff405ad660f2ab5e413ced21b606c1f
SerjRide <quicksquicks6@gmail.com>
2020-07-16 12:43:49 +0300

    add changes

--- src/components/add-todos/add-todos.js
+++ src/components/add-todos/add-todos.js
@@ -5,24 +5,39 @@ import './add-todos.css';
 export default class AddTodos extends Component {
 
   state = {
+    label: ''
+  }
 
-  };
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdd(this.state.label);
+    this.setState({
+      label: ''
+    })
+  }
 
 
   render() {
-    const { onAdd } = this.props;
-    const txt = 'New Task';
 
     return (
-      <div className="add-todos">
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right"
-                onClick={ () => onAdd(txt) }>
+      <form className="add-todos form-inline"
+            onSubmit={ this.onSubmit }>
+        <input  type="text"
+                className="form-control"
+                onChange={ this.onLabelChange }
+                placeholder="What needs to be done?"
+                value={ this.state.label }/>
+        <button className="btn btn-outline-info btn-sm float-right">
           <i className ="fa fa-plus" aria-hidden="true"></i>
         </button>
-      </div>
+      </form>
     );
   };
-
 
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,11 +13,17 @@ export default class App extends Component {
 
   maxId = 100;
 
+  createTodoItem = (label) => {
+    return {
+      label, important: false, done: false, id: this.maxId++
+    }
+  }
+
   state = {
     todoDate: [
-      { label: 'Drink Coffee', important: false, id: 1 },
-      { label: 'Make Awesome App', important: true, id: 2 },
-      { label: 'Have a lunch', important: false, id: 3 }
+      this.createTodoItem('Drink Cofee'),
+      this.createTodoItem('Make Awesome App'),
+      this.createTodoItem('Have a lunch')
     ]
   };
 
@@ -28,16 +34,12 @@ export default class App extends Component {
         ...todoDate.slice(0, idx),
         ...todoDate.slice(idx + 1)
       ];
-      return {
-        todoDate: newArr
-      };
+      return { todoDate: newArr };
     });
   };
 
   addItem = (txt) => {
-    const newItem = {
-      label: txt, important: false, id: this.maxId++
-    };
+    const newItem = this.createTodoItem(txt);
 
     this.setState(({ todoDate }) => {
       const newArr = [...todoDate, newItem];
@@ -45,13 +47,46 @@ export default class App extends Component {
     });
   };
 
+  toggleProperty(arr, id, propName) {
+      const idx = arr.findIndex((el) => el.id === id);
+
+      const oldItem = arr[idx];
+      const newItem = {
+        ...oldItem,
+        [propName]: !oldItem[propName]
+      };
+
+      return [
+        ...arr.slice(0, idx),
+        newItem,
+        ...arr.slice(idx + 1)
+      ];
+  };
+
+  onToggleImportant = (id) => {
+    this.setState(({ todoDate }) => {
+      return {
+        todoDate: this.toggleProperty(todoDate, id, 'important')
+      }
+    });
+  };
+
+  onToggleDone = (id) => {
+    this.setState(({ todoDate }) => {
+      return {
+        todoDate: this.toggleProperty(todoDate, id, 'done')
+      }
+    });
+  };
+
   render() {
     const { todoDate } = this.state;
-
+    const doneCount = todoDate.filter((el) => el.done).length
+    const todoCount = todoDate.length - doneCount;
 
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={ todoCount } done={ doneCount } />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
@@ -59,7 +94,9 @@ export default class App extends Component {
 
         <TodoList
           todos={ todoDate }
-          onDeleted = { this.deleteItem }/>
+          onDeleted = { this.deleteItem }
+          onToggleImportant = { this.onToggleImportant }
+          onToggleDone = { this.onToggleDone }/>
         <AddTodos
           onAdd={ this.addItem }/>
       </div>
